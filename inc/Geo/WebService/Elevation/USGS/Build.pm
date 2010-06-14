c76ffd1136f320d2d8b4a7311ade2804555ccf5f
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2010-06-14 22:33:34 +0000

    Add authortest tests to run the normal tests without the optional modules.
    git-svn-id: file:///Users/tom/Code/svn/repos/Geo-WebService-Elevation-USGS/trunk@1796 f6fd3633-1913-47c2-bad4-372d75c948ec

--- inc/Geo/WebService/Elevation/USGS/Build.pm
+++ inc/Geo/WebService/Elevation/USGS/Build.pm
@@ -5,16 +5,110 @@ use warnings;
 
 use base qw{ Module::Build };
 
-our $VERSION = '0.004';
-
 use Carp;
+use File::Spec;
 
+my @optionals_dir = qw{ xt optionals };
+my @hide = qw{ Time::HiRes };
+
+{
+    my $done;
+    my $hider;
+
+    sub _get_hider {
+	$done and return $hider;
+	$done = 1;
+	foreach my $module ( qw{
+		Test::Without::Module
+		Devel::Hide
+	    } ) {
+	    eval "require $module; 1"
+		and return ( $hider = $module );
+	}
+	return $hider;
+    }
+}
+
+sub _get_tests_without_optional_modules {
+    my @args = @_;
+    _get_hider() or return;
+    my @cleanup;
+    @args or @args = _get_general_tests();
+    foreach my $path ( @args ) {
+	push @cleanup, File::Spec->catfile( @optionals_dir,
+	    ( File::Spec->splitpath( $path ) )[2] );
+    }
+    return @cleanup;
+}
+
+{
+
+    my @general_tests;
+
+    sub _get_general_tests {
+	@general_tests and return @general_tests;
+	my $th;
+	opendir $th, 't'
+	    or die "Unable to open directory t: $!\n";
+	while ( defined( my $fn = readdir $th ) ) {
+	    '.' eq substr $fn, 0, 1 and next;
+	    my $path = File::Spec->catfile( 't', $fn );
+	    -f $path or next;
+	    push @general_tests, $path;
+	}
+	closedir $th;
+	return @general_tests;
+    }
+}
+
+
+sub ACTION_make_optional_modules_tests {
+    my ( $self, @args ) = @_;
+
+    my $hider = _get_hider() or do {
+#	warn "Neither Devel::Hide nor Test::Without::Module available\n";
+	return;
+    };
+
+    my $gendir = File::Spec->catdir( @optionals_dir );
+
+    -d $gendir
+	or mkdir $gendir
+	or die "Unable to create $gendir: $!\n";
+
+    foreach my $ip ( _get_general_tests() ) {
+	my ( $op ) = _get_tests_without_optional_modules( $ip );
+	-f $op and next;
+	local $/ = undef;
+	open my $ih, '<', $ip or die "Unable to open $ip: $!\n";
+	my $content = <$ih>;
+	close $ih;
+	print "Creating $op\n";
+	open my $oh, '>', $op or die "Unable to open $op: $!\n";
+	print { $oh } <<EOD;
+package main;
+
+use strict;
+use warnings;
+
+use $hider qw{ @hide };
+
+require '$ip';
+EOD
+	close $oh;
+    }
+}
 
 sub ACTION_authortest {
     my ( $self, @args ) = @_;
 
-    $self->depends_on( 'build' );
-    $self->test_files( qw{ t xt } );
+    my @depends_on = ( qw{ build make_optional_modules_tests } );
+    -e 'META.yml' or push @depends_on, 'distmeta';
+    $self->depends_on( @depends_on );
+    my @test_files = qw{ t xt };
+    my $optdir = File::Spec->catdir( @optionals_dir );
+    -d $optdir and push @test_files, $optdir;
+    $self->test_files( @test_files );
     $self->depends_on( 'test' );
 
     return;
@@ -26,7 +120,7 @@ __END__
 
 =head1 NAME
 
-Geo::WebService::Elevation::USGS::Build - Extend Module::Build for PPIx::Regexp
+Geo::WebService::Elevation::USGS::Build - Extend Module::Build for Geo::WebService::Elevation::USGS
 
 =head1 SYNOPSIS
 
@@ -42,10 +136,11 @@ This extension of L<Module::Build|Module::Build> adds the following
 action to those provided by L<Module::Build|Module::Build>:
 
   authortest
+  make_optional_modules_tests
 
 =head1 ACTIONS
 
-This module provides the following action:
+This module provides the following actions:
 
 =over
 
@@ -63,6 +158,18 @@ are not present.
 
 This test is sensitive to the C<verbose=1> argument, but not to the
 C<--test_files> argument.
+
+=item make_optional_modules_tests
+
+This action creates the tests in the F<xt/optionals> directory. These
+generally duplicate the tests in the F<t> directory, but the optional
+modules are made unavailable using either
+L<Test::Without::Module|Test::Without::Module> or
+L<Devel::Hide|Devel::Hide>, in that order. If neither of these modules
+is available, nothing is done.
+
+There should be no need to invoke this action directly, since the
+C<authortest> action depends on it.
 
 =back
 
