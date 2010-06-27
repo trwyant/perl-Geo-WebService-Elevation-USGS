e58b1e307ac93f02745bfd3418673585801155d0
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2010-06-27 06:14:22 +0000

    Consolidate dependencies into inc/.../Meta.pm. Also have this know what version of Perl we want, and (via environment variable MAKING_MODULE_DISTRIBUTION) whether we are making a distribution, so I can have Build.PL tell Module::Build that it is not needed for configuring, since the user can always use ExtUtils::MakeMaker and Makefile.PL. The corresponding functionality for EUMM was not implemented, since I build distributions with Module::Build.
    git-svn-id: file:///Users/tom/Code/svn/repos/Geo-WebService-Elevation-USGS/trunk@1809 f6fd3633-1913-47c2-bad4-372d75c948ec

---
+++ inc/Geo/WebService/Elevation/USGS/Meta.pm
@@ -0,0 +1,132 @@
+package Geo::WebService::Elevation::USGS::Meta;
+
+use 5.008;
+
+use strict;
+use warnings;
+
+use Carp;
+
+sub new {
+    my ( $class ) = @_;
+    ref $class and $class = ref $class;
+    my $self = {
+	distribution => $ENV{MAKING_MODULE_DISTRIBUTION},
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub distribution {
+    my ( $self ) = @_;
+    return $self->{distribution};
+}
+
+sub requires {
+    my ( $self, @extra ) = @_;
+##  if ( ! $self->distribution() ) {
+##  }
+    return {
+	'Params::Util'	=> 0.11,
+	'Scalar::Util'	=> 1.10,
+	'SOAP::Lite'	=> 0,
+	@extra,
+    };
+}
+
+sub requires_perl {
+    return 5.008;
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Geo::WebService::Elevation::USGS::Meta - Information needed to build Geo::WebService::Elevation::USGS
+
+=head1 SYNOPSIS
+
+ use lib qw{ inc };
+ use Geo::WebService::Elevation::USGS::Meta;
+ my $meta = Geo::WebService::Elevation::USGS::Meta->new();
+ use YAML;
+ print "Required modules:\n", Dump(
+     Geo::WebService::Elevation::USGS::Meta->requires() );
+
+=head1 DETAILS
+
+This module centralizes information needed to build
+C<Geo::WebService::Elevation::USGS>.
+
+=head1 METHODS
+
+This class supports the following public methods:
+
+=head2 new
+
+This method instantiates the class.
+
+=head2 distribution
+
+This method returns the value of the environment variable
+C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
+
+=head2 requires
+
+ Geo::WebService::Elevation::USGS::Meta->requires();
+
+This static method computes and returns a reference to a hash describing
+the modules required to run the C<Geo::WebService::Elevation::USGS>
+package, suitable for use in a F<Build.PL> C<requires> key, or a
+F<Makefile.PL> C<PREREQ_PM> key. Any additional arguments will be
+appended to the generated hash. In addition, unless
+L<distribution()|/distribution> is true, configuration-specific modules
+may be added.
+
+=head2 requires_perl
+
+ Geo::WebService::Elevation::USGS::Meta->requires_perl();
+
+This static method returns the version of Perl required by the package.
+
+=head1 ATTRIBUTES
+
+This class has no public attributes.
+
+
+=head1 ENVIRONMENT
+
+=head2 MAKING_MODULE_DISTRIBUTION
+
+This environment variable should be set to a true value if you are
+making a distribution. This ensures that no configuration-specific
+information makes it into F<META.yml>.
+
+
+=head1 SUPPORT
+
+Support is by the author. Please file bug reports at
+L<http://rt.cpan.org>, or in electronic mail to the author.
+
+=head1 AUTHOR
+
+Thomas R. Wyant, III F<wyant at cpan dot org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010, Thomas R. Wyant, III
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses in the directory LICENSES.
+
+This program is distributed in the hope that it will be useful, but
+without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
+# ex: set textwidth=72 :
