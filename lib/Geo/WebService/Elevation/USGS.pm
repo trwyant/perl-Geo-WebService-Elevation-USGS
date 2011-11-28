436d0991aebf33d9e5a85ba1c19148b8f4498273
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2011-11-28 00:26:20 +0000

    Update version to 0.009. Update Changes file for changes since 0.008.
    git-svn-id: file:///Users/tom/Code/svn/repos/Geo-WebService-Elevation-USGS/trunk@2713 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Geo/WebService/Elevation/USGS.pm
+++ lib/Geo/WebService/Elevation/USGS.pm
@@ -108,7 +108,7 @@ use LWP::UserAgent;
 use Scalar::Util 1.10 qw{ blessed looks_like_number };
 use XML::Parser;
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 use constant BEST_DATA_SET => -1;
 
