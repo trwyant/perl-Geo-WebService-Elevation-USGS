f44a711735ac0e32b97ebc196fef10d512096810
Tom Wyant <wyant@cpan.org>
2019-06-21 11:14:14 -0400

    Version 0.113
    Set version to 0.113. Update Changes file for changes since v00.112_01.

--- lib/Geo/WebService/Elevation/USGS.pm
+++ lib/Geo/WebService/Elevation/USGS.pm
@@ -127,7 +127,7 @@ use JSON;
 use LWP::UserAgent;
 use Scalar::Util 1.10 qw{ blessed looks_like_number };
 
-our $VERSION = '0.112_01';
+our $VERSION = '0.113';
 
 use constant BEST_DATA_SET => -1;
 use constant USGS_URL => 'http://ned.usgs.gov/epqs/pqs.php';
