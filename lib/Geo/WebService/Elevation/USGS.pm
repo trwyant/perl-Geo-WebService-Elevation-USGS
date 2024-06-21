324df2d147545ab93c5756bbbc7b89408e7fb048
Tom Wyant <wyant@cpan.org>
2024-06-21 12:30:58 -0400

    Version 0.200
    Set version to 0.200. Update Changes file for changes since v00.199_02.

--- lib/Geo/WebService/Elevation/USGS.pm
+++ lib/Geo/WebService/Elevation/USGS.pm
@@ -78,7 +78,7 @@ use JSON;
 use LWP::UserAgent;
 use Scalar::Util 1.10 qw{ blessed looks_like_number };
 
-our $VERSION = '0.199_02';
+our $VERSION = '0.200';
 
 # use constant USGS_URL => 'https://ned.usgs.gov/epqs/pqs.php';
 # use constant USGS_URL => 'https://nationalmap.gov/epqs/pqs.php';
